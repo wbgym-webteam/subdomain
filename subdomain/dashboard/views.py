20639ab3893515697d143a0924eab3baef9827d1
micha003 <michael.vorogushyn@wbgym.de>
2024-10-29 18:23:41 +0100

    added login app, created db model for login code

--- subdomain/dashboard/views.py
+++ subdomain/dashboard/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 # Create your views here.
-def login(request):
-    if request.method == "POST":
-        return redirect("home")
-    return render(request, "login.html")
 
 
 def home(request):
--- subdomain/subdomain/urls.py
+++ subdomain/subdomain/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("dashboard.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("dashboard.urls")),
+    path("", include("login.urls")),
+]
