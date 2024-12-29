430d6ed81294592bd7e51a97fcdc3433569784fe
Morrígan <phantom.morrigan@proton.me>
2024-12-29 16:34:48 +0100

    remove old code

--- subdomain/app.py
+++ subdomain/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
 import sqlite3 
+from migrations import migrate
 
 db = sqlite3.connect("wbgym.db")
+migrate(db)
 
 app = Flask(__name__)
 
@@ -9,3 +12,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.get("/home")
+def home():
+    return render_template("home.html")
--- subdomain_old/GameOfGrapes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GameofgrapesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GameOfGrapes'
