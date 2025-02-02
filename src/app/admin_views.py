ff719e010742be2ffdda1ed950f78db9f0bed1fa
micha003 <michael.vorogushyn@wbgym.de>
2025-02-02 15:41:02 +0100

    deleted duplication in routing

--- src/app/admin_views.py
+++ src/app/admin_views.py
@@ -11,14 +11,6 @@ from flask import (
 admin_views = Blueprint("admin_views", __name__, static_folder="static")
 
 
-@admin_views.route("/admin_login", methods=["GET", "POST"])
-def adminLogin():
-    if request.method == "POST":
-        pass
-    else:
-        return render_template("admin/admin_login.html")
-
-
-@admin_views.route("/")
+@admin_views.route("/admin_dashboard")
 def adminDashboard():
     return render_template("admin/admin_dashboard.html")
