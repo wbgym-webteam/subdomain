c53656bf5dbbf1292724ef2f2212431e47fbdb09
micha003 <michael.vorogushyn@wbgym.de>
2025-02-06 15:15:00 +0100

    finished the filehandler for the tdw admin panel

--- src/app/tdw_filehandler.py
+++ src/app/tdw_filehandler.py
@@ -28,6 +28,25 @@ def addStudent(ENGINE, student_id, last_name, first_name, grade):
     session.close()
 
 
+def addPresentation(ENGINE, presentation_id, title, presenter, abstract, grades):
+    new_presentation = Presentation(
+        presentation_id=presentation_id,
+        title=title,
+        presenter=presenter,
+        abstract=abstract,
+        grades=grades,
+    )
+
+    # Create a new session
+    Session = sessionmaker(bind=ENGINE)
+    session = Session()
+
+    # Add and commit the new student to the database
+    session.add(new_presentation)
+    session.commit()
+    session.close()
+
+
 def FileHandler(file):
     ENGINE = create_engine(DB_URL)
     db.metadata.create_all(ENGINE)
@@ -41,12 +60,39 @@ def FileHandler(file):
             sheet1.iter_rows(min_row=2, values_only=True), start=2
         ):
 
-            id_value = row[0]  # Column A (ID)
+            student_id = row[0]  # Column A (ID)
             last_name = row[1]  # Column B (Last Name)
             first_name = row[2]  # Column C (First Name)
             grade = row[4]  # Column E (Grade)
 
-            addStudent(ENGINE, id_value, last_name, first_name, grade)
+            addStudent(ENGINE, student_id, last_name, first_name, grade)
+    except IntegrityError:
+        pass
+
+    # Get the Presentations
+    sheet2 = workbook.worksheets[2]
+
+    try:
+        for row_index, row in enumerate(
+            sheet2.iter_rows(min_row=2, values_only=True), start=2
+        ):
+            presentation_id = row[0]
+            title = row[1]
+            presenter = row[2]
+            abstract = row[3]
+
+            grades = []
+            g = 5
+            for grade in row[4:11]:
+                if grade == -1:
+                    grades.append(g)
+                else:
+                    pass
+                g += 1
+
+            grades = str(grades)[1:-1]
+
+            addPresentation(EGNINE, presentation_id, title, presenter, abstract, grades)
     except IntegrityError:
         pass
 
