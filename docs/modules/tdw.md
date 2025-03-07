1d24c01eaae5d4dd1903ebf3509dc232792ebe69
micha003 <michael.vorogushyn@wbgym.de>
2025-03-07 23:38:43 +0100

    worked on docs

--- docs/modules/tdw.md
+++ docs/modules/tdw.md
@@ -4,5 +4,28 @@ This module is for an event called "Tag der Wissenschaften" (Day of Science) at 
 
 ## Structure
 
-The Login is happening at `auth.py`, where you can choose to login either to the TdW or the [SmS](sms.md) Module. When the students logged in successfully, they will be redirected to `/tdw`. 
-This is where the presentation selection process happens.
+The Login is happening at `auth.py`, where you can choose to login either to the TdW or the [SmS](sms.md) Module. When the students logged in successfully, they will be redirected to `/tdw`. This is where the presentation selection process happens.
+
+There the students can select mulitple presentations from a list of the available ones which are for his grade. When ready, the student submits the selected presentations and afterwards he can logout.
+
+The administration of the TdW module is done in the admin panel. There you can import the presentations and student lists from an `.xlsx`-file. The admin panel automatically creates the necessary database entries and generates the logincodes for the students. The logincodes can be exported to a zip file which contains a `.docx`-file for each class. When you have done all the necessary preperation, you can now activate the module for the students.
+
+**List of auth & admin files**:
+
+- `auth.py`
+- `admin_views.py`
+
+_Templates:_
+
+- `login.html`
+- `/admin/`
+
+_The models for the DB are found in `models.py`._
+
+**List of module files**:
+
+- `tdw_views.py`
+- `tdw_filehandler.py`
+- `tdw_logincode_export.py`
+
+_The templates are found in `/templates/tdw/`._
--- src/app/tdw_views.py
+++ src/app/tdw_views.py
@@ -1,38 +1,56 @@
+# Module Import
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, session
 import sqlalchemy
 from sqlalchemy import text
 from .models import Student, Presentation
 
+# Importing DB for sqlalchemy
 from . import db
 
+# Blueprint
 tdw = Blueprint("tdw", __name__)
+
+# ---------------------------------------------------------------
+# TDW Routes
+# ---------------------------------------------------------------
+#
+# This is the view that the logged in student will see
 
 
 @tdw.route("/", methods=["GET", "POST"])
 def selection():
+    # Checks if the user is logged in
     if session.get("logged_in", True) == True:
         if request.method == "POST":
-            pass
+            pass  # idk you can delete this but be careful pls :D
         else:
-            # Get the student
+            # Get the student id from the session storage
             student_id = session.get("tdw_student_id")
             if student_id is None:
                 return redirect("/login")
             student_id = str(student_id)
+
+            # DB Query to get the student's name and grade
             db_current_student = db.session.execute(
                 db.select(
                     Student.first_name, Student.last_name, Student.grade
                 ).filter_by(id=student_id)
             ).one_or_none()
 
+            # It creates a dictoionary from the query result to access the values by key
             current_student = dict(db_current_student._mapping)
 
+            # Extraction of the student's name for display on the template
             full_name = (
                 f'{current_student["first_name"]} {current_student["last_name"]}'
             )
+
+            # The grade is relevant for the options that will be displayed
             grade = current_student["grade"]
 
             # Get the presentations
+            #
+            # So the dictionary will be having the presentation_id as the key and the value will be a list of the presentation's title, presenter and abstract
             presentations_dict = dict()
             presentations_list = list()
 
@@ -43,11 +61,14 @@ def selection():
                     presentations_list.append(str(presentation[0].title))
                     presentations_list.append(str(presentation[0].presenter))
                     presentations_list.append(str(presentation[0].abstract))
+                    # Ik it is kinda goofy but it works, so you need to customize the string yk
                     presentation_id = str(presentation[0]).split()[1][0:-1]
+                    # Here it enters the presentation (list) into the dictionary
                     presentations_dict[presentation_id] = presentations_list
+                    # Now it clears up the list for the next presentation
                     presentations_list = list()
 
-            # Get the chosen presentations
+            # Get the ALREADY chosen presentations
             chosen_presentations = list()
             results = db.session.execute(
                 text(
@@ -61,6 +82,7 @@ def selection():
                 for result in results:
                     chosen_presentations.append(str(result[0]))
 
+            # When rendering the template, the student's name, the presentations and the already chosen presentations are passed as arguments, so they will be displayed on the site
             return render_template(
                 "tdw/tdw_selection.html",
                 student=full_name,
