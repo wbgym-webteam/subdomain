5885cf33030b2d2b93ea825e86f31bde48e5ea4e
micha003 <michael.vorogushyn@wbgym.de>
2025-03-08 12:52:10 +0100

    i have done sth

--- src/app/tdw_logincode_export.py
+++ src/app/tdw_logincode_export.py
@@ -1,6 +1,8 @@
 from docx import Document
+from docx.shared import Pt
 from sqlalchemy import text
 import zipfile
+import os
 import os
 
 from . import db
@@ -15,8 +17,17 @@ def get_class(grade, grade_selector):
     return query
 
 
+def get_sek2(grade):
+    query = db.session.execute(
+        text(
+            f"SELECT first_name, last_name, logincode FROM students WHERE grade = {grade} ORDER BY last_name, first_name"
+        )
+    ).fetchall()
+    return query
+
+
 def zip_files():
-    directory = "data/tdw/downloads"
+    directory = "./app/data/tdw/downloads"
     with zipfile.ZipFile("data/tdw/downloads/TdW_Logincodes.zip", "w") as zipf:
         for file in os.listdir(directory):
             if file.endswith(".docx"):
@@ -24,27 +35,59 @@ def zip_files():
 
 
 def export_logincodes():
-    for grade in range(5, 12, 1):
+    print(os.getcwd())
+    output_dir = os.getenv("OUTPUT_DIR", "./app/data/tdw/downloads")
+
+    for grade in range(5, 13, 1):
         for grade_selector in range(1, 5, 1):
-            print(f"Exporting grade {grade}/{grade_selector}")
-            query = getClass(grade, grade_selector)
+            if grade == 11 or grade == 12 or grade == 5 or grade == 6:
+                print(f"Exporting grade {grade}")
+                query = get_sek2(grade)
+            else:
+                print(f"Exporting grade {grade}/{grade_selector}")
+                query = get_class(grade, grade_selector)
+
             if len(query) > 0 or query is not None:
                 doc = Document()
-                doc.add_heading(f"TDW Login Codes {grade}/{grade_selector}", 0)
-                for row in query:
-                    table = doc.add_table(rows=len(query), cols=3)
-                    hdr_cells = table.rows[0].cells
-                    hdr_cells[0].text = "First Name"
-                    hdr_cells[1].text = "Last Name"
-                    hdr_cells[2].text = "Login Code"
-                    for row in query:
-                        row_cells = table.add_row().cells
-                        row_cells[0].text = row.first_name
-                        row_cells[1].text = row.last_name
-                        row_cells[2].text = row.logincode
-                doc.save(
-                    f"/app/data/tdw/downloads/TdW_Logincodes_{grade}_{grade_selector}.docx"
-                )
-                print(f"Finished grade {grade}/{grade_selector}")
+                if grade == 11 or grade == 12 or grade == 5 or grade == 6:
+                    doc.add_heading(f"TDW Login Codes {grade}", 0)
+                else:
+                    doc.add_heading(f"TDW Login Codes {grade}/{grade_selector}", 0)
 
-    zip_files()
+                table = doc.add_table(rows=1, cols=3)
+                table.style = "Table Grid"
+
+                headers = ["Header 1", "Header 2", "Header 3"]
+                for i, header in enumerate(headers):
+                    cell = table.cell(0, i)
+                    cell.text = header
+
+                    # Set font size and make it bold
+
+                    for paragraph in cell.paragraphs:
+                        run = paragraph.runs[0]
+                        run.bold = True
+                        run.font.size = Pt(14)
+
+                hdr_cells = table.rows[0].cells
+                hdr_cells[0].text = "First Name"
+                hdr_cells[1].text = "Last Name"
+                hdr_cells[2].text = "Login Code"
+                for row in query:
+                    row_cells = table.add_row().cells
+                    row_cells[0].text = row.first_name
+                    row_cells[1].text = row.last_name
+                    row_cells[2].text = row.logincode
+
+                if grade == 11 or grade == 12 or grade == 5 or grade == 6:
+                    doc.save(os.path.join(output_dir, f"TdW_Logincodes_{grade}.docx"))
+                    print(f"Finished grade {grade}")
+                else:
+                    doc.save(
+                        os.path.join(
+                            output_dir, f"TdW_Logincodes_{grade}_{grade_selector}.docx"
+                        )
+                    )
+                    print(f"Finished grade {grade}/{grade_selector}")
+
+    # zip_files()
