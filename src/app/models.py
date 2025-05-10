191affdeb5843a1d94fd0cb3752413ad02b9caa5
Z0mbieNick <nicolas.evdokimov@icloud.com>
2025-05-10 14:53:42 +0200

    added models to the SMS module

--- src/app/models.py
+++ src/app/models.py
@@ -44,3 +44,55 @@ class BlockedPresentation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, nullable=False)
     presentation_id = db.Column(db.Integer, nullable=False)
+
+
+
+### # ------------------------------------------------------
+# sms
+
+class StudentSMS(db.Model):
+    __tablename__ = "students_sms"
+
+    Student_id = db.Column(db.Integer, primary_key=True)
+    last_name = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    grade = db.Column(db.Integer, nullable=False)
+    grade_selector = db.Column(db.Integer, nullable=False)
+    logincode = db.Column(db.String(20), nullable=False)
+    
+    #relationship for the Student_course table
+    courses_in_Student_course = db.relationship("Course", secondary="Student_course", backref="students_in_Student_course")
+
+    #relationship for the Host table
+    courses_in_Host = db.relationship("Course", secondary="Hosts", backref="students_in_Hosts")
+
+
+
+class Student_course(db.Model):
+    __tablename__ = "tudent_course"
+
+    Student_id = db.Column(db.Integer, db.ForeignKey("students_sms.Student_id"), primary_key=True)
+    Course_id = db.Column(db.Integer, db.ForeignKey("courses.course_id"), primary_key=True)
+    weight = db.Column(db.Integer, nullable=False)
+
+
+
+class Hosts(db.Model):
+    __tablename__ = "hosts"
+
+    Student_id = db.Column(db.Integer, db.ForeignKey("students_sms.Student_id"), primary_key=True)
+    Course_id = db.Column(db.Integer, db.ForeignKey("courses.course_id"), primary_key=True)
+
+
+class Course(db.Model):
+    __tablename__ = "courses"
+
+    course_id = db.Column(db.Integer, primary_key=True)
+    course_title = db.Column(db.String, nullable=False)
+    course_private = db.Column(db.Boolean, nullable=False)
+    couse_description = db.Column(db.String, nullable=False)
+    course_teacher = db.Column(db.String, nullable=False)
+
+
+
+
